7f6b6cbb0783b86718785d2dda2d2236f1c67386
Milywayz <jtjenks@gmail.com>
2023-04-13 21:24:51 -0500

    added basic html layout and css. Also added two querySelectors in JS

--- javascript.js
+++ javascript.js
@@ -1,24 +1,44 @@
+let searchButton = document.querySelector('#searchButton')
+let searchCity = document.querySelector('#searchCity')
 
 
-fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=c59796062f55d4612d60a1d4b41769ea")
-    .then(response => response.json())
-    .then(cityFound => {
 
-        let firstCity = cityFound[0];
-        console.log(firstCity.lat);
-        console.log(firstCity.lon);
-        console.log(cityFound);
 
-        return fetch("http://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=c59796062f55d4612d60a1d4b41769ea")
-    })
 
-    .then(response => response.json())
-    .then(data => {
+
+
+// Add eventLisnter 
+
+
+
+searchButton.addEventListener('click' , function(event){
+    event.preventDefault()
+
+    let inputCity = searchCity.ariaValueMax;
+    localStorage.setItem("savedCity", inputCity)
+
+
+
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=Fort-Worth&limit=5&appid=c59796062f55d4612d60a1d4b41769ea")
+        .then(response => response.json())
+        .then(cityFound => {
     
-    console.log(data);
-
-
-    })
+            let firstCity = cityFound[0];
+            console.log(firstCity.lat);
+            console.log(firstCity.lon);
+            console.log(cityFound);
+    
+            return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=c59796062f55d4612d60a1d4b41769ea`)
+        })
+    
+        .then(response => response.json())
+        .then(data => {
+        
+        console.log(data);
+    
+    
+        })
+})
 
 
 
