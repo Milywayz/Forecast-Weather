c39bee198b17dfe33406f20fa2405f01d888e6df
Milywayz <jtjenks@gmail.com>
2023-04-16 10:00:54 -0500

    Creating my own cards in the for loop and worked on localstorage for my ul

--- javascript.js
+++ javascript.js
@@ -3,9 +3,8 @@ let searchCity = document.querySelector('#searchCity')
 let cityTemp1 = document.querySelectorAll('.card-title')
 let cityWind1 = document.querySelectorAll('.card-subtitle')
 let cityHumidity1 = document.querySelectorAll('.card-text')
-
-
-
+let weather = document.querySelector('.weather')
+let allCities = [];
 
 
 
@@ -19,7 +18,33 @@ let cityHumidity1 = document.querySelectorAll('.card-text')
 //     let searchCityVal = searchCity.value
 //     let inputCity = searchCityVal
     let inputCity = "Dallas"
+    function storage(){
+        const cities = localStorage.getItem("inputCity")
+
+        if (cities != null){
+            let citesParsed = JSON.parse(cities)
+            citesParsed.push(inputCity)
+            localStorage.setItem('inputCity' , JSON.stringify(citesParsed))
+        }
+        else {
     
+            localStorage.setItem('inputCity' , JSON.stringify(inputCity))
+        }
+        
+            
+    }
+    function displayCities() {
+
+        const cities = localStorage.getItem("inputCity")
+        if(cities == null) return; 
+
+        const parsedCities = JSON.parse(cities)
+        for (let i = 0; i < parsedCities.length; i++) {
+            
+            
+
+        }
+    }
 
 
 
@@ -40,9 +65,14 @@ let cityHumidity1 = document.querySelectorAll('.card-text')
         console.log(cityInfo);
         console.log(cityInfo.city.name)
             
-        let cityName = document.createElement('h2')
+        let cityName = document.querySelector('#cityName')
         cityName.textContent = cityInfo.city.name
-        divContainer.appendChild(cityName)
+        // divContainer.appendChild(cityName)
+
+        let todaysDate = document.querySelector('.todaysDate')
+        todaysDate.textContent = dayjs().format('MMM DD, YYYY')
+        
+
 
         let cityTemp = document.createElement('p')
                 let cityWind = document.createElement('p')
@@ -59,24 +89,43 @@ let cityHumidity1 = document.querySelectorAll('.card-text')
 
             for (let i = 0; i < cityList.length; i+=8) {
                 
-                // let cityTemp = document.createElement('p')
-                // let cityWind = document.createElement('p')
-                // let cityHumidity = document.createElement('p')
+                let cityTemp = document.createElement('p')
+                let cityWind = document.createElement('p')
+                let cityHumidity = document.createElement('p')
+    
 
-                // cityTemp.textContent = cityList[i].main.temp
-                // cityWind.textContent = cityList[i].wind.speed
-                // cityHumidity.textContent = cityList[i].main.humidity
+                cityTemp.textContent = cityList[i].main.temp
+                cityWind.textContent = cityList[i].wind.speed
+                cityHumidity.textContent = cityList[i].main.humidity
+
+                let cards = document.createElement('div')
+                cards.classList.add('card')
         
+                let cardBody = document.createElement('div')
+                cardBody.classList.add('card-body')
 
-                cityTemp1.textContent = "Temp: " + cityList[i].main.temp + "F"
-                cityWind.textContent = "Wind Speed: " +cityList[i].wind.speed
-                cityHumidity1.textContent = "Humidity: " +cityList[i].main.humidity + "%"
+                let titleCard = document.createElement('h5')
+                titleCard.classList.add('card-title')
+                titleCard.textContent = dayjs(cityList[i].dt_txt).format('MMM DD, YYYY')
+
+                let cardSubtitle = document.createElement('h5')
+                cardSubtitle.classList.add('card-subtitle')
+                cardSubtitle.textContent = "Temp: " + cityList[0].main.temp + "F"
+
+
+                cards.appendChild(cardSubtitle)
+                cards.appendChild(cardBody)
+                cards.appendChild(titleCard)
+                weather.appendChild(cards)
+
+                // console.log(cityTemp1)
+                // cityTemp1.textContent = "Temp: " + cityList[i].main.temp + "F"
+                // cityWind.textContent = "Wind Speed: " +cityList[i].wind.speed
+                // cityHumidity1.textContent = "Humidity: " +cityList[i].main.humidity + "%"
                 //cardBody.textContent = moment(cityList[i], "X").format("MM/DD/YYYY HH:mm:ss") 
 
                
 
-                // divContainer.appendChild(cityWind)
-                // divContainer.appendChild(cityHumidity)
                 
 
 
