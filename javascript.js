18b9dd05ec719ac0f7aab98fbf00093937f62516
Milywayz <jtjenks@gmail.com>
2023-04-16 18:33:11 -0500

    FInisehd on the cards for now, working on adding a ul of past searches

--- javascript.js
+++ javascript.js
@@ -5,6 +5,7 @@ let cityWind1 = document.querySelectorAll('.card-subtitle')
 let cityHumidity1 = document.querySelectorAll('.card-text')
 let weather = document.querySelector('.weather')
 let allCities = [];
+let ul = document.querySelector('.showPastCities')
 
 
 
@@ -13,41 +14,48 @@ let allCities = [];
 
 
 
-// searchButton.addEventListener('click' , function(){
+searchButton.addEventListener('click' , function(){
     
-//     let searchCityVal = searchCity.value
-//     let inputCity = searchCityVal
-    let inputCity = "Dallas"
-    function storage(){
-        let cities = localStorage.getItem("inputCity")
-
-        if (cities != null){
-            let citesParsed = JSON.parse(cities)
-            citesParsed.push(inputCity)
-            localStorage.setItem('inputCity' , JSON.stringify(citesParsed))
-        }
-        else {
     
-            localStorage.setItem('inputCity' , JSON.stringify(inputCity))
-        }
+    let searchCityVal = searchCity.value
+    let inputCity = searchCityVal
+    // let inputCity = "Dallas"
+    // function storage(){
+    //     let cities = localStorage.getItem("inputCity")
         
+    //     if (cities != null){
+    //         let citesParsed = JSON.parse(cities)
+    //         citesParsed.push(inputCity)
+    //         localStorage.setItem('inputCity' , JSON.stringify(citesParsed))
+    //     }
+    //     else {
             
-    }
-    function displayCities() {
-
-        let cities = localStorage.getItem("inputCity")
-        if(cities == null) return; 
-
-        let parsedCities = JSON.parse(cities)
-        for (let i = 0; i < parsedCities.length; i++) {
+    //         localStorage.setItem('inputCity' , JSON.stringify(inputCity))
+    //     }
+        
+        
+    // }
+    // function displayCities() {
+        
+    //     let cities = localStorage.getItem("inputCity")
+    //     if(cities == null) return; 
+        
+    //     let parsedCities = JSON.parse(cities)
+    //     for (let i = 0; i < parsedCities.length; i++) {
+            
+    //         let liCities = document.createElement('button')
+            
+    //         liCities.textContent = parsedCities[i]
             
             
-
-        }
-    }
-
-
-
+    //         ul.appendChild(liCities)
+    //     }
+    // }
+    
+    // storage();
+    // displayCities();
+    
+    
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&limit=5&appid=c59796062f55d4612d60a1d4b41769ea`)
         .then(response => response.json())
         .then(cityFound => {
@@ -78,8 +86,8 @@ let allCities = [];
                 let cityWind = document.createElement('p')
                 let cityHumidity = document.createElement('p')
 
-                cityTemp.textContent = "Temp: " + cityList[0].main.temp + "F"
-                cityWind.textContent = "Wind Speed: " +cityList[0].wind.speed
+                cityTemp.textContent = "Temp: " + cityList[0].main.temp + "°F"
+                cityWind.textContent = "Wind Speed: " +cityList[0].wind.speed + "MPH"
                 cityHumidity.textContent = "Humidity: " +cityList[0].main.humidity + "%"
 
 
@@ -110,42 +118,34 @@ let allCities = [];
 
                 let cardSubtitle = document.createElement('h5')
                 cardSubtitle.classList.add('card-subtitle')
-                cardSubtitle.textContent = "Temp: " + cityList[0].main.temp + "F"
+                cardSubtitle.textContent = "Temp: " + cityList[i].main.temp + "°F"
 
                 let mb2 = document.createElement('h5')
                 mb2.classList.add('mb-2')
-                mb2.textContent =  "Wind Speed: " +cityList[0].wind.speed
+                mb2.textContent =  "Wind Speed: " + cityList[i].wind.speed + "MPH"
 
                 let cardText = document.createElement('h5')
                 cardText.classList.add('card-text')
-                cardText.textContent = "Humidity: " +cityList[0].main.humidity + "%"
+                cardText.textContent = "Humidity: " + cityList[i].main.humidity + "%"
 
-
-
+                let cardLink = document.createElement('h5')
+                cardLink.classList.add('card-link')
+                cardLink.textContent = "Skys: " + cityList[i].weather[0].main
 
                 cards.appendChild(titleCard)
+                cards.appendChild(cardLink)
                 cards.appendChild(cardSubtitle)
                 cards.appendChild(cardText)
-                cards.appendChild(cardBody)
                 cards.appendChild(mb2)
+                cards.appendChild(cardBody)
                 weather.appendChild(cards)
 
-                // console.log(cityTemp1)
-                // cityTemp1.textContent = "Temp: " + cityList[i].main.temp + "F"
-                // cityWind.textContent = "Wind Speed: " +cityList[i].wind.speed
-                // cityHumidity1.textContent = "Humidity: " +cityList[i].main.humidity + "%"
-                //cardBody.textContent = moment(cityList[i], "X").format("MM/DD/YYYY HH:mm:ss") 
-
-               
-
                 
-
-
-            }
+            } 
             
     
     })
-// })
+})
 
 
 
